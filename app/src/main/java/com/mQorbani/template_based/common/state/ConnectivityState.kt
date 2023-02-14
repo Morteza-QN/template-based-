d2906f8630fd7879d6f184a8242b48d25c28a737
M.Qorbani <mortezaghorbani3@gmail.com>
2023-02-14 10:02:29 +0330

    ConnectivityState add

---
+++ app/src/main/java/com/mQorbani/template_based/common/state/ConnectivityState.kt
@@ -0,0 +1,36 @@
+package com.mQorbani.template_based.common.state
+
+import android.app.Application
+import android.content.Context
+import android.net.*
+import androidx.lifecycle.LiveData
+
+class ConnectivityState(private val cm: ConnectivityManager) : LiveData<Boolean>() {
+
+    constructor(application: Application) : this(
+        application.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    )
+
+    private val networkCallback = object : ConnectivityManager.NetworkCallback() {
+        override fun onAvailable(network: Network) {
+            super.onAvailable(network)
+            postValue(true)
+        }
+
+        override fun onLost(network: Network) {
+            super.onLost(network)
+            postValue(false)
+        }
+    }
+
+    override fun onActive() {
+        super.onActive()
+        val request = NetworkRequest.Builder()
+        cm.registerNetworkCallback(request.build(), networkCallback)
+    }
+
+    override fun onInactive() {
+        super.onInactive()
+        cm.unregisterNetworkCallback(networkCallback)
+    }
+}
