d18ca786782d59bba9bb6426805b67f784c3c4da
M.Qorbani <mortezaghorbani3@gmail.com>
2023-02-14 07:39:27 +0330

    first Commit

---
+++ app/src/main/java/com/mQorbani/template_based/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mQorbani.template_based
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
